57e49f18a797691864b9a18129595b215889d530
Shipra Oberoi <oberoishipra@gmail.com>
2025-08-31 09:04:08 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>AI RFP Prototype</h1>
+      <p>Welcome to the prototype app.</p>
+    </div>
+  )
+}
+
+export default App
